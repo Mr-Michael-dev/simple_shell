cf49c406a5bf68765473fac7be2eb30af94e9517
GODSPE1 <godspy18@gmail.com>
2023-11-06 06:38:32 -0800

    create header file and necessary files

---
+++ shell.h
@@ -0,0 +1,11 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+#endif /*SHELL_H*/
