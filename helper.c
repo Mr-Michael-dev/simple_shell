463ef5647d08a78fc28b7c233f16f74cedd63337
GODSPE1 <godspy18@gmail.com>
2023-11-06 22:24:59 -0800

    add memory deallocaing function

---
+++ helper.c
@@ -0,0 +1,30 @@
+#include "shell.h"
+/**
+ * free_mem - free the  allocated memory
+ * @arg: string of arguments
+ *
+ * Return: Nothing
+ */
+void free_mem(char **arg)
+{
+	int i;
+
+	for (i = 0; arg[i]; i++)
+	{
+		if  (arg[i] != NULL)
+			free(arg[i]);
+	}
+
+	if (arg != NULL)
+		free (arg);
+}
+
+/**
+ *
+ *
+ *
+ */
+char tokenizer(char *)
+{
+
+}
