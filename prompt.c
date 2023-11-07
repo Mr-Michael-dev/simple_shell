63b12c86a4fe8556bababeda021170629c8ffa31
GODSPE1 <godspy18@gmail.com>
2023-11-06 22:24:04 -0800

    add tokenizer

--- prompt.c
+++ prompt.c
@@ -2,7 +2,7 @@
 
 /**
  * main - entrypoint to the shell
- * 
+ *
  * Return: 0 always
  */
 
@@ -10,7 +10,9 @@ int main(void)
 {
 	char *line_buf = NULL;
 	size_t buf_len = 0;
-	char *line_cpy;
+	char *line_cpyi, token;
+	int counter;
+	char **argv;
 	/*pid_t child_pid;*/
 
 	printf("#gmsh$ ");
@@ -21,6 +23,17 @@ int main(void)
 		printf("%s\n", line_cpy);
 
 		printf("#gmsh$ ");
+		argv = (10 * sizeof(char *));
+		token = strtok(lin_buff, " \n");
+		while (token != NULL)
+		{
+			argv[i] = malloc(sizeof(char *) * (strlen(token) + 1));
+			token = str
+				conter++;
+			token = strtok(NULL, " \n");
+		}
+		argv[i] = NULL;
+
 		free(line_buf);
 		free(line_cpy);
 		line_buf = NULL;
