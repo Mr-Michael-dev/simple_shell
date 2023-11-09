95b3d010d9a1022c3b51ae8eee77202d20d738f2
Mr-Michael-dev <michael.oyedepo@gmail.com>
2023-11-09 00:38:57 -0800

    create custom getenv function

---
+++ _getenv.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+extern char **environ;
+
+char *_getenv(const char *name)
+{
+	int i;
+	char *token;
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		token = strtok(environ[i], "=");
+
+		if (token == NULL)
+		{
+			continue;
+		}
+
+		if (strcmp(name, token) == 0)
+		{
+			token = strtok(NULL, " \n");
+			return (token);
+		}
+	}
+
+	return (NULL);
+
+}
