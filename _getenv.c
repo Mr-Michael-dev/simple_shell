b0936f4fbe590a245185c024b83b40f254262d19
GODSPE1 <godspy18@gmail.com>
2023-11-10 02:40:15 -0800

    edit prompt.c file

--- _getenv.c
+++ _getenv.c
@@ -1,4 +1,5 @@
-#include "shell.h"
+#include <stdio.h>
+#include <string.h>
 
 extern char **environ;
 
--- prompt.c
+++ prompt.c
@@ -5,54 +5,66 @@
  *
  * Return: 0 always
  */
-
 int main(void)
 {
-	char *line_buf = NULL, *line_cpy;
-	size_t buf_len = 0;
-	char **arg;
-	char *arg_delim = " \n";
-	char **full_path;
-	int i;
+        char *line_buf = NULL, *line_cpy;
+        size_t buf_len = 0;
+        char **arg;
+        pid_t child_pid;
 
-	if (isatty(STDIN_FILENO))
-	{
-		printf("#gmsh$ ");
-	}
-	while ((getline(&line_buf, &buf_len, stdin)) != EOF)
-	{
-		line_cpy = strdup(line_buf);
+        if (isatty(STDIN_FILENO))
+        {
+                printf("#gmsh$ ");
+        }
+        while ((getline(&line_buf, &buf_len, stdin)) != EOF)
+        {
+                line_cpy = strdup(line_buf);
+                /******* 
+                  if (line_cpy == NULL)
+                  {
+                  perror("strdup");
+                  exit(EXIT_FAILURE);
+                  }
+                 ********/
 
-		arg = tokenize(line_cpy, arg_delim);
-		if (arg == NULL || strspn(line_cpy, " \t\r\n") == strlen(line_cpy))
-		{
-			free(line_cpy);
-			free_mem(arg);
-		}
-		else
-		{
-			full_path = getpath(arg[0]);
-			for (i = 0; full_path[i]; i++)
-			{
-				if (access(full_path[i], X_OK) == 0)
-				{
-					child_process(full_path[i], arg);
-				}
-			}
-			perror("access");
-			free_mem(arg);
-			free_mem(full_path);
-			free(line_buf);
-			free(line_cpy);
-			line_buf = NULL;
-			buf_len = 0;
-		}
-		if (isatty(STDIN_FILENO))
-			printf("#gmsh$ ");
-	}
+                arg = tokenize(line_cpy);
+                if (arg == NULL || strspn(line_cpy, " \t\r\n") == strlen(line_cpy))
+                {
+                        free(line_cpy);
+                        free_mem(arg);
+                }
+                else
+                {
+                        child_pid = fork();
+                        if (child_pid == -1) 
+                        {
+                                perror("fork");
+                                free_mem(arg);
+                                free(line_cpy);
+                                exit(EXIT_FAILURE);
+                        }
+                        if (child_pid == 0)
+                        {
+                                execute(arg);
+                                exit(EXIT_SUCCESS);
+                        }
+                        else
+                        {
+                                wait(NULL);
+                        }
 
-	free(line_buf);
-	if (isatty(STDIN_FILENO))
-		printf("\n");
-	return (0);
+                        free_mem(arg);
+                        free(line_buf);
+                        free(line_cpy);
+                        line_buf = NULL;
+                        buf_len = 0;
+                }
+                if (isatty(STDIN_FILENO))
+                        printf("#gmsh$ ");
+        }
+
+        free(line_buf);
+        if (isatty(STDIN_FILENO))
+                printf("\n");
+        return (0);
 }
