6c2d90a008516a513e7561bb38255e9a5d22d3c0
Mr-Michael-dev <michael.oyedepo@gmail.com>
2023-11-16 10:40:38 -0800

    debug change_directory.c

--- change_directory.c
+++ change_directory.c
@@ -7,9 +7,22 @@
  */
 void _chdir(char **arg)
 {
+	char *home;
+
 	if (arg[1] == NULL)
 	{
-		chdir(_getenv("HOME"));
+		home = _getenv("HOME");
+		if (home == NULL)
+		{
+			fprintf(stderr, "cd: HOME not set\n");
+			return;
+		}
+
+		if (chdir(home) != 0)
+		{
+			fprintf(stderr, "cd: %s: %s\n", arg[1], strerror(errno));
+		}
+		free(home);
 	}
 	else
 	{
